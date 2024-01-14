15e0e748751b9f5bd691291d0cf654a6599c663c
David Lee <ing.jdplee@gmail.com>
2024-01-14 14:34:21 -0400

    Step: Organizations

---
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,60 @@
+import { Plus } from "lucide-react";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+
+export const Navbar = () => {
+  return (
+    <nav className="fixed top-0 z-50 flex items-center w-full px-4 bg-white border-b shadow-sm h-14">
+      {/* TODO: Mobile Sidebar */}
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+        <Button
+          variant="primary"
+          size="sm"
+          className="rounded-sm hidden md:block h-auto  py-1.5 px-2"
+        >
+          Create
+        </Button>
+        <Button
+          variant="primary"
+          size="sm"
+          className="block rounded-sm md:hidden"
+        >
+          <Plus className="w-4 h-4" />
+        </Button>
+      </div>
+      <div className="flex items-center ml-auto gap-x-2">
+        <OrganizationSwitcher
+          hidePersonal
+          afterCreateOrganizationUrl="/organization/:id"
+          afterLeaveOrganizationUrl="/select-org"
+          afterSelectOrganizationUrl="/organization/:id"
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              },
+            },
+          }}
+        />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: {
+                height: 30,
+                width: 30,
+              },
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
+};
